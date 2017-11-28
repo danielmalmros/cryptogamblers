c2d7b7fa9e13452cec27ee3e34d67459e13c8060
Rasmus Lagoni <rasmus191190@live.dk>
2017-11-28 09:51:11 +0100

    Mortens changes

--- cryptoGamblers/cryptoGamblers/Models/AccountViewModels.cs
+++ cryptoGamblers/cryptoGamblers/Models/AccountViewModels.cs
@@ -3,41 +3,41 @@ using System.ComponentModel.DataAnnotations;
 
 namespace cryptoGamblers.Models
 {
-    public class ExternalLoginConfirmationViewModel
-    {
-        [Required]
-        [Display(Name = "Email")]
-        public string Email { get; set; }
-    }
+    //public class ExternalLoginConfirmationViewModel
+    //{
+    //    [Required]
+    //    [Display(Name = "Email")]
+    //    public string Email { get; set; }
+    //}
 
-    public class ExternalLoginListViewModel
-    {
-        public string ReturnUrl { get; set; }
-    }
+    //public class ExternalLoginListViewModel
+    //{
+    //    public string ReturnUrl { get; set; }
+    //}
 
-    public class SendCodeViewModel
-    {
-        public string SelectedProvider { get; set; }
-        public ICollection<System.Web.Mvc.SelectListItem> Providers { get; set; }
-        public string ReturnUrl { get; set; }
-        public bool RememberMe { get; set; }
-    }
+    //public class SendCodeViewModel
+    //{
+    //    public string SelectedProvider { get; set; }
+    //    public ICollection<System.Web.Mvc.SelectListItem> Providers { get; set; }
+    //    public string ReturnUrl { get; set; }
+    //    public bool RememberMe { get; set; }
+    //}
 
-    public class VerifyCodeViewModel
-    {
-        [Required]
-        public string Provider { get; set; }
+    //public class VerifyCodeViewModel
+    //{
+    //    [Required]
+    //    public string Provider { get; set; }
 
-        [Required]
-        [Display(Name = "Code")]
-        public string Code { get; set; }
-        public string ReturnUrl { get; set; }
+    //    [Required]
+    //    [Display(Name = "Code")]
+    //    public string Code { get; set; }
+    //    public string ReturnUrl { get; set; }
 
-        [Display(Name = "Remember this browser?")]
-        public bool RememberBrowser { get; set; }
+    //    [Display(Name = "Remember this browser?")]
+    //    public bool RememberBrowser { get; set; }
 
-        public bool RememberMe { get; set; }
-    }
+    //    public bool RememberMe { get; set; }
+    //}
 
     public class ForgotViewModel
     {
@@ -66,7 +66,7 @@ namespace cryptoGamblers.Models
     {
         [Required(ErrorMessage = "bobobobob den skal udfyldes yo")]
         [StringLength(50, MinimumLength = 4, ErrorMessage = "den skal være mellem 4 og 50 yo")]
-        [Display(Name = "A possibly not unique username")]
+        [Display(Name = "Username")]
         public string Username { get; set; }
 
         [Required]
