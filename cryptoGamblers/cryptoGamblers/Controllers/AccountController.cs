c4b0c5e9545ca891522ab4fbd0acad992cd17135
Michael Lauridsen <Pjuker@gmail.com>
2017-11-30 10:01:24 +0100

    Added managebalance

--- cryptoGamblers/cryptoGamblers/Controllers/AccountController.cs
+++ cryptoGamblers/cryptoGamblers/Controllers/AccountController.cs
@@ -151,7 +151,7 @@ namespace cryptoGamblers.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Username, Email = model.Email };
+                var user = new ApplicationUser { UserName = model.Username, Email = model.Email, Balance = 100 };
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
