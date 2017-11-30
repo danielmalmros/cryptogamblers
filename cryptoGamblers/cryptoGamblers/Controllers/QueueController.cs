40564979e88265886933ca0478e2914944b09803
stausholm <muffix26@gmail.com>
2017-11-30 10:14:23 +0100

    Ninject

---
+++ cryptoGamblers/cryptoGamblers/Controllers/QueueController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace cryptoGamblers.Controllers
+{
+    public class QueueController : Controller
+    {
+
+
+        public QueueController()
+        {
+
+        }
+        // GET: Queue
+        public ActionResult QueueMe() {
+            var userName = this.User.Identity.Name;
+
+            //service.findopponent(userName)
+            
+            return View();
+        }
+    }
+}
