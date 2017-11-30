f737a3e375792d55c0297424eb60b5e27cf96cde
Rasmus Lagoni <rasmus191190@live.dk>
2017-11-30 09:00:12 +0100

    Email replaced by userName in login flow

--- cryptoGamblers/cryptoGamblers/Models/AccountViewModels.cs
+++ cryptoGamblers/cryptoGamblers/Models/AccountViewModels.cs
@@ -49,9 +49,9 @@ namespace cryptoGamblers.Models
     public class LoginViewModel
     {
         [Required]
-        [Display(Name = "Email")]
+        [Display(Name = "UserName")]
         [EmailAddress]
-        public string Email { get; set; }
+        public string UserName{ get; set; }
 
         [Required]
         [DataType(DataType.Password)]
