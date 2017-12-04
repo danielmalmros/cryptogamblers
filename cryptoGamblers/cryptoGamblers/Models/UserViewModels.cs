f0161ef3e673f8144028eb2a1ada1cce3be5d493
Michael Lauridsen <pjuker@gmail.com>
2017-12-04 21:20:27 +0100

    Added initial functionality for user profile page

---
+++ cryptoGamblers/cryptoGamblers/Models/UserViewModels.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace cryptoGamblers.Models
+{
+
+    public class UserProfileViewModel
+    {
+        [Display(Name = "Username")]
+        public string Username { get; set; }
+
+        [Display(Name = "Profile picture")]
+        public string AvatarPath { get; set; }
+
+        [Display(Name = "Profile picture")]
+        public string ProfileDescription { get; set; }
+    }
+}
