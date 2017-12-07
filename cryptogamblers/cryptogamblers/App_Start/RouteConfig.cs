ba0923f6bd7395a40fb7b0ddf74fd5b422cdc84b
Michael Lauridsen <Pjuker@gmail.com>
2017-12-07 09:54:16 +0100

    changed routing for userprofiles

--- cryptogamblers/cryptogamblers/App_Start/RouteConfig.cs
+++ cryptogamblers/cryptogamblers/App_Start/RouteConfig.cs
@@ -7,17 +7,18 @@ using System.Web.Routing;
 
 namespace cryptoGamblers
 {
-    public class RouteConfig
-    {
-        public static void RegisterRoutes(RouteCollection routes)
-        {
-            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+	public class RouteConfig
+	{
+		public static void RegisterRoutes(RouteCollection routes)
+		{
+			routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Account", action = "Login", id = UrlParameter.Optional }
-            );
-        }
-    }
+			routes.MapRoute(
+				name: "Default",
+				url: "{controller}/{action}/{id}",
+				defaults: new { controller = "Account", action = "Login", id = UrlParameter.Optional }
+			);
+
+		}
+	}
 }
