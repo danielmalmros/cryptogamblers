603f2cb6900d4fc975b63a1e864c444cf75893dd
Rasmus Lagoni <rasmus191190@live.dk>
2017-12-10 15:48:44 +0100

    commit

--- cryptoGamblers/cryptoGamblers/Controllers/QueueController.cs
+++ cryptoGamblers/cryptoGamblers/Controllers/QueueController.cs
@@ -17,47 +17,97 @@ namespace cryptoGamblers.Controllers
         private ApplicationDbContext db = new ApplicationDbContext();
 
         // GET: Queue
-        public Models.ActionResult<Match> QueueMe() {
+        public JsonResult QueueMe() {
+
             var userName = User.Identity.GetUserName();
             var userId = User.Identity.GetUserId();
+            var response = new Models.ActionResult<Match>();
 
             var queue = db.queueIn.FirstOrDefault(u => u.Opponent2 == null);
+
             if (queue == null)
             {
                 queue = db.queueIn.Create();
                 queue.Opponent1 = userName;
                 db.queueIn.AddOrUpdate(queue);
-            } else
+                db.SaveChanges();
+            }
+            else
             {
+
                 if (queue.Opponent1 != userName)
                 {
                     queue.Opponent2 = userName;
                     db.queueIn.AddOrUpdate(queue);
+                    db.SaveChanges();
+                    bool MatchTransfered = false;
+                    //get newly created match
+                    while (!MatchTransfered)
+                    {
+                        //re-init application context so data is updated
+                        db = new ApplicationDbContext();
+                        var matchCreated = db.Match.FirstOrDefault(u => u.Opponent2 == userName && u.Opponent1 == queue.Opponent1);
+
+                        if (matchCreated != null)
+                        {
+                            MatchTransfered = true;
+                            
+                             response = new Models.ActionResult<Match>
+                            {
+                                Object = matchCreated,
+                                Status = new HttpStatusCodeResult(200)
+                            };
+                            return Json(response, JsonRequestBehavior.AllowGet);
+
+                        }
+                        else
+                        {
+                            System.Threading.Thread.Sleep(1000);
+                        }
+                    }
                 }
             }
-            db.SaveChanges();
 
-            bool queueContains = db.queueIn.Any(t => t.Opponent1 != null && t.Opponent2 != null);
+            //Waiting for opponent
+            bool OpponentFound = false;
+                while (!OpponentFound)
+                {
+                //re-init application context so data is updated
+                db = new ApplicationDbContext();
+                queue = db.queueIn.FirstOrDefault(u => u.Opponent1 == userName);
+                    if (!string.IsNullOrEmpty(queue.Opponent2))
+                    {
+                        OpponentFound = true;
+                    }
+                    else
+                    {
+                        System.Threading.Thread.Sleep(1000);
+                    }
+            }
+
+            bool queueContains = db.queueIn.Any(t => t.Opponent1 != null && t.Opponent2 != null && t.Opponent1 == userName);
+            
+            //transfer from queue to actual match
             Match newMatch = null;
-
             if (queueContains)
             {
                 QueueIn queueData = db.queueIn.Where(x => x.Opponent1 == userName || x.Opponent2 == userName).Select(x => x).FirstOrDefault();
-                 newMatch = new Match { Opponent1 = queueData.Opponent1, Opponent2 = queueData.Opponent2 };
+                newMatch = new Match { Opponent1 = queueData.Opponent1, Opponent2 = queueData.Opponent2, Date = DateTime.Now };
                 db.Match.AddOrUpdate(newMatch);
 
                 newMatch = db.Match.FirstOrDefault(x => x.Opponent1 == newMatch.Opponent1 && x.Opponent2 == newMatch.Opponent2);
 
-
                 db.queueIn.Remove(queue);
-
                 db.SaveChanges();
             }
 
-            return  new Models.ActionResult<Match>{
+            response = new Models.ActionResult<Match>
+            {
                 Object = newMatch,
                 Status = new HttpStatusCodeResult(200)
             };
+
+            return Json(response, JsonRequestBehavior.AllowGet);
         }
     }
 }
