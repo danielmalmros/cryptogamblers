899be690144d5b1f634b69f9ca440ed859bdc724
Daniel Malmros Christiansen <daniel@bymalmros.dk>
2017-12-12 10:38:45 +0100

    added script in bundle

---
+++ cryptogamblers/cryptogamblers/Scripts/queue/queue.js
@@ -0,0 +1,10 @@
+﻿$(".game-button").click(function () {
+    $(".game-button").css("display", "none");
+    $("#loader").css("display", "block");
+    $.ajax({
+        url: "/queue/QueueMe", success: function (result) {
+            var redirectUrl = "http://localhost:49816/Match/Index?id=" + result.Object.MatchId;
+            window.location = redirectUrl;
+        }
+    });
+});
